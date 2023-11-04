dcc1bd2fd8f89caf8deeebe3798dbd02a69f7791
Sonny Bizure <sonnybizure@gmail.com>
2023-11-03 23:13:10 -0400

    Website in progress

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    const menu=document.querySelector(".menu-links"); 
+    const icon=document.querySelector(".hamburger-links"); 
+    menu.classList.toggle("open");
+    menu.classList.toggle("open");
+}
